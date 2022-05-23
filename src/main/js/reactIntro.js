add6feb3ba0bce9b7ae4d4682816db4c60f07de5
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-05-23 16:06:40 -0500

    creating project to practice React

---
+++ src/main/js/reactIntro.js
@@ -0,0 +1,19 @@
+(() => {
+    "use strict";
+
+    const myReactComponent = React.createElement("h1", {}, "I am creating my first React component");
+
+    console.log(myReactComponent);
+
+    const myNestedReactComponent = React.createElement("div", {}, React.createElement("p", {}, "This element is nested"));
+
+    console.log(myNestedReactComponent);
+
+    // const App = React.createElement("h1", {}, "Our First React page has rendered");
+    // const App = myReactComponent;
+    const App = myNestedReactComponent;
+    ReactDOM.render(App, document.getElementById("root"));
+
+})();
+
+
