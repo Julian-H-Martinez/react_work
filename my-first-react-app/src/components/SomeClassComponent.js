dfbbd3e213254008f3a7cb957e9cab2a2bbfd460
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-05-31 16:40:33 -0500

    finished understanding Children of a component and how I can use them

--- my-first-react-app/src/components/SomeClassComponent.js
+++ my-first-react-app/src/components/SomeClassComponent.js
@@ -4,12 +4,20 @@ class SomeClassComponent extends Component{
     render(){
         //  this refers to SomeClassComponent
         //  props refers to properties that can be passed
-        const {firstName, lastName} = this.props;
+        // const {firstName, lastName} = this.props;
+        const {header, children} = this.props;
         return (
+            // <div>
+            //     <h1>This is our first class component!</h1>
+            //     <h3>Welcome, {firstName} {lastName}</h3>
+            // </div>
             <div>
-                <h1>This is our first class component!</h1>
-                <h3>Welcome, {firstName} {lastName}</h3>
-            </div>)
+                <h1>
+                    {header}
+                </h1>
+                {children}
+            </div>
+        )
     }
 }
 
