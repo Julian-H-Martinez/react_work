dc17644aa37741da16e02a53777a893167aa7d4b
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-05-31 16:15:46 -0500

    added 2 lines of comments

--- my-first-react-app/src/components/SomeClassComponent.js
+++ my-first-react-app/src/components/SomeClassComponent.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 
 class SomeClassComponent extends Component{
     render(){
+        //  this refers to SomeClassComponent
+        //  props refers to properties that can be passed
         const {firstName, lastName} = this.props;
         return (
             <div>
