11182935e476a598f7327a53c657166114baf5c8
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-06-06 11:41:59 -0500

    added Navigation panel; styling is minimal to get all parts on page once all parts are on page will style accordingly

--- my-portfolio/src/components/App/App.js
+++ my-portfolio/src/components/App/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from '../Header';
+import Navigation from '../Navigation';
 
 
 function App() {
   return (
     <div className="app">
       <Header />
-      {/*<Navigation />*/}
+      <Navigation />
       {/*<Main>*/}
       {/*  <SubContents />*/}
       {/*  <SubContents />*/}
